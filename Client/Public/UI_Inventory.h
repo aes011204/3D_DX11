e86ccd20042b572d583e28e06032ba7cfc73ed55
aes011204 <aes011204@gmail.com>
2026-03-05 00:12:01 +0900

    chore:

--- Client/Public/UI_Inventory.h
+++ Client/Public/UI_Inventory.h
@@ -1,44 +1,44 @@
 #pragma once
 #include "UIPanel.h"
-//class CUI_Inventory :
-//    public CUIPanel
-//{
-//public:
-//    struct _DESC : public CUIPanel::UIPANEL_DESC
-//    {
-//        /* _uint TextureComLevel = {};
-//         _wstring TextureProtoName = L"";*/
-//
-//         //_bool IsTrnasparent = {};
-//        //_bool IsFullScreen = {};
-//        //_bool IsUseLayout = {};
-//    };
-//private:
-//    CUI_Inventory(ComPtr<ID3D11Device> pDevice, ComPtr<ID3D11DeviceContext> pContext);
-//    CUI_Inventory(const CUIPanel& prototype);
-//public:
-//    ~CUI_Inventory() = default;
-//
-//public:
-//
-//    HRESULT OnInit(void* pArg) override;
-//    void OnActive()override;
-//    void OnInActive()override;
-//    void OnDisabled()override;
-//    void OnUpdate(const _float& timeDelta)override;
-//    void OnLateUpdate()override;
-//    HRESULT OnRender()override;
-//    void OnClear()override;
-//
-//public:
-//
-//private:
-//
-//    _bool m_IsTrnasparent = {};
-//    _bool m_IsFullScreen = {};
-//    _bool m_IsUseLayout = {};
-//public:
-//   // static shared_ptr<CUI_MainMenu> Create(ComPtr<ID3D11Device> pDevice, ComPtr<ID3D11DeviceContext> pContext);
-//    void Free() override;
-//};
-//
+class CUI_Inventory :
+    public CUIPanel
+{
+public:
+    struct INVENTORY_DESC : public CUIPanel::UIPANEL_DESC
+    {
+        /* _uint TextureComLevel = {};
+         _wstring TextureProtoName = L"";*/
+
+         //_bool IsTrnasparent = {};
+        //_bool IsFullScreen = {};
+        //_bool IsUseLayout = {};
+    };
+private:
+    CUI_Inventory(ComPtr<ID3D11Device> pDevice, ComPtr<ID3D11DeviceContext> pContext);
+    CUI_Inventory(const CUIPanel& prototype);
+public:
+    ~CUI_Inventory() = default;
+
+public:
+
+    HRESULT OnInit(void* pArg) override;
+    void OnActive()override;
+    void OnInActive()override;
+    void OnDisabled()override;
+    void OnUpdate(const _float& timeDelta)override;
+    void OnLateUpdate()override;
+    HRESULT OnRender()override;
+    void OnClear()override;
+
+public:
+
+private:
+
+    _bool m_IsTrnasparent = {};
+    _bool m_IsFullScreen = {};
+    _bool m_IsUseLayout = {};
+public:
+    static shared_ptr<CUI_Inventory> Create(ComPtr<ID3D11Device> pDevice, ComPtr<ID3D11DeviceContext> pContext);
+    void Free() override;
+};
+
